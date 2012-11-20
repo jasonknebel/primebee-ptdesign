8a26b1eb3d6b80c8032be83c7a4f3a565367d958
Jason Knebel <jasonkneb@gmail.com>
2012-11-19 21:15:00 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Primebee::Application.config.secret_token = '16a216a9aab98ccfb5b1dbc0a249238e11f76c875b5148c22a8583c67768868d9e11e70f25aae5954d35db21446f2b70df5717f129aa6445726e90ee33ae206f'
